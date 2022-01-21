cbb2e547bbf86ba80a979b598fe02c990aadc4d3
OUTHAMI Hassan <outhami0hassan@gmail.com>
2022-01-21 19:29:27 +0100

    third commit modification DP1StrategyPattern

--- src/main/java/org/designePattern/DP1StrategyPattern/Duck/Duck.java
+++ src/main/java/org/designePattern/DP1StrategyPattern/Duck/Duck.java
@@ -3,29 +3,15 @@ package org.designePattern.DP1StrategyPattern.Duck;
 import org.designePattern.DP1StrategyPattern.behaviour.IFlyBehaviour;
 import org.designePattern.DP1StrategyPattern.behaviour.IDisplayBehaviour;
 import org.designePattern.DP1StrategyPattern.behaviour.IEatBehaviour;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Duck {
 IFlyBehaviour flyBehaviour;
 IEatBehaviour eatBehaviour;
 IDisplayBehaviour displayBehaviour;
-
-public Duck(IFlyBehaviour flyBehaviour,IEatBehaviour eatBehaviour,IDisplayBehaviour displayBehaviour) {
-	this.displayBehaviour=displayBehaviour;
-	this.eatBehaviour=eatBehaviour;
-	this.flyBehaviour=flyBehaviour;
-			
-}
-
-public IFlyBehaviour getFlyBehaviour() {
-	return flyBehaviour;
-}
-
-public IEatBehaviour getEatBehaviour() {
-	return eatBehaviour;
-}
-
-public IDisplayBehaviour getDisplayBehaviour() {
-	return displayBehaviour;
-}
-
 }
